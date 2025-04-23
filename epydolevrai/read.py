107c3c74fad173acf66cde9c32b6e50a5451e551
melena dijoux <melena.dijoux@epitech.eu>
2025-04-23 16:00:06 +0400

    add

--- epydolevrai/read.py
+++ epydolevrai/read.py
@@ -10,11 +10,7 @@
 
 import csv
 
-# Ouverture du fichier CSV
 with open('bs.csv', 'r') as csvfile:
-    # Création de l'objet reader
-    reader = csv.reader(file, delimiter=',', quotechar='|')
-    for row in reader: print(row)
-        if row(id) == id
-        # Affichage id dans le terminal
-        print(f"ID: {ligne[0]}, Nom: {ligne[1]}, Email: {ligne[2]}")
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in reader:
+        print(row)
