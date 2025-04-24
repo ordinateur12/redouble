856cb369a59929571364bfeb5227510123abd1ca
melena dijoux <melena.dijoux@epitech.eu>
2025-04-24 16:18:33 +0400

    main

--- epydolevrai/read.py
+++ epydolevrai/read.py
@@ -11,12 +11,22 @@
 import csv
 import sys
 
-if (sys.argv[1] == "get") :
-    id = sys.argv[3]
-with open('bs.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
+def get(id, reader):
     for row in reader:
         if (id == row['id']) :
             print(f"id : {row['id']}")
             print(f"name : {row['name']}")
             print(f"email : {row['email']}")
+
+def delete():
+    
+
+if __name__ == '__main__':
+    with open('bs.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if (sys.argv[1] == "get") :
+            id = sys.argv[3]
+            get(id, reader)
+        if (sys.argv[1] == "del") :
+            id = sys.argv[3]
+            delete(id)
