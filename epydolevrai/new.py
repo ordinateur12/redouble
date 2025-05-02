4ef81815c07e0e7bb81e9693f1b728e9b740f521
melena dijoux <melena.dijoux@epitech.eu>
2025-05-02 11:17:13 +0400

    delete

---
+++ epydolevrai/new.py
@@ -0,0 +1,4 @@
+import os
+
+def get_csv_path(resource):
+    return os.path.join(f"{resource}.csv")
--- epydolevrai/read.py
+++ epydolevrai/read.py
@@ -10,6 +10,8 @@
 
 import csv
 import sys
+import pprint
+from new import get_csv_path
 
 def get(id, reader):
     for row in reader:
@@ -18,15 +20,29 @@ def get(id, reader):
             print(f"name : {row['name']}")
             print(f"email : {row['email']}")
 
-def delete():
-    
+def delete(id, data):
+    fieldnames = ['id', 'name', 'email']
+    tab = []
+    for row in data:
+        if (id == row['id']):
+            print(f"delete  user {id}")
+            continue
+        tab.append(row)
+    with open('bs.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(tab)
 
-if __name__ == '__main__':
+def opn():
     with open('bs.csv', 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-        if (sys.argv[1] == "get") :
-            id = sys.argv[3]
-            get(id, reader)
-        if (sys.argv[1] == "del") :
-            id = sys.argv[3]
-            delete(id)
+        return list(reader)
+
+if __name__ == '__main__':
+    data = opn()
+    if (sys.argv[1] == "get") :
+        id = sys.argv[3]
+        get(id, data)
+    if (sys.argv[1] == "del") :
+        id = sys.argv[3]
+        delete(id, data)
